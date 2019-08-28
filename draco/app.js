eae9f6ce600c455bc1da458bfd1d8f52e65ca942
dougsillars <doug.sillars@gmail.com>
2019-08-28 13:27:57 +0200

    venus resize

--- draco/app.js
+++ draco/app.js
@@ -15,7 +15,7 @@
 
 const MODEL_OBJ_URL = '../assets/ArcticFox_Posed.obj';
 const MODEL_MTL_URL = '../assets/ArcticFox_Posed.mtl';
-const MODEL_SCALE = 0.5;
+const MODEL_SCALE = 0.1;
 const MODEL_GLTF = '../assets/daviddraco/daviddraco.gltf';
 
 /**
